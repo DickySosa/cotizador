c03989681d826527bf60705f16995219bcc86123
DickySosa <dicky5217@gmail.com>
2023-10-29 10:11:44 -0600

    adding function to the region combination

--- src/app/components/package-info/package-info.component.ts
+++ src/app/components/package-info/package-info.component.ts
@@ -1,13 +1,13 @@
 import { Component } from '@angular/core';
 
-interface PackageInfo{
-  height:string;
-  weight:string;
-  length:string;
-  width:string;
-  origin:string;
-  destination:string;
-  discountCode?:string;
+interface PackageInfo {
+  height: string;
+  weight: string;
+  length: string;
+  width: string;
+  origin: string;
+  destination: string;
+  discountCode?: string;
 }
 @Component({
   selector: 'app-package-info',
@@ -23,31 +23,50 @@ export class PackageInfoComponent {
     width: '',
     origin: '',
     destination: '',
-    discountCode:''
+    discountCode: ''
   };
 
 
-  finalPrice!:number;
-  discountPrice!:number;
+  finalPrice!: number;
+  discountPrice!: number;
 
+  vec1: string[] = ["Asia", "Africa", "Norteamérica", "Europa", "Oceanía", "Sudamérica", "Antártida", "Regiones polares", "Centroamérica"];
 
-  handleClick (){
+  ngOnInit() {
 
-    console.log("hola")
+    function getCombinations(arr: string[]): string[][] {
+      const combinations: string[][] = [];
+    
+      for (let i = 0; i < arr.length; i++) {
+        for (let j = i; j < arr.length; j++) {
+          combinations.push([`${arr[i]} - ${arr[j]}`]);
+        }
+      }
+      return combinations;
+    }
+    
+    const combinations = getCombinations(this.vec1);
+    
+    console.log(combinations);
+  }
+
+  handleClick() {
+
+    console.log(this.packageInfo)
 
 
   }
 
-  calculateDiscount(originalPrice:number, clientLevel:string):number{
+  calculateDiscount(originalPrice: number, clientLevel: string): number {
 
-    if(clientLevel === 'premium'){
-      let discount:number| null = null; 
+    if (clientLevel === 'premium') {
+      let discount: number | null = null;
       discount = originalPrice * 0.10;
-      return this.discountPrice =  originalPrice - discount
-    } else if (clientLevel === 'vip'){
-      let discount:number| null = null; 
+      return this.discountPrice = originalPrice - discount
+    } else if (clientLevel === 'vip') {
+      let discount: number | null = null;
       discount = originalPrice * 0.15;
-      return this.discountPrice =  originalPrice - discount
+      return this.discountPrice = originalPrice - discount
     }
     return originalPrice
   }
